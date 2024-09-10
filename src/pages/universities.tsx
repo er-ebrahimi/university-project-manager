ef44171cb66ad2eed9a55f522b88bdab38f3afc5
Aref alijannezhad <74285590+Arefalj@users.noreply.github.com>
2024-09-10 22:24:07 +0330

    fix ui univercity page

--- src/pages/universities.tsx
+++ src/pages/universities.tsx
@@ -28,7 +28,7 @@ export default function Universities() {
 
   return (
     <>
-      <div className="flex items-center overflow-auto">
+      <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">دانشگاه‌ها</h1>
       </div>
       <div
